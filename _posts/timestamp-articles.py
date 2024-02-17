74f2e061936613569ded2a6a2b279c82ea0ce119
An Rodriguez <anmichel@gmail.com>
2024-02-17 17:06:57 -0500

    pseudo working version

--- _posts/timestamp-articles.py
+++ _posts/timestamp-articles.py
@@ -1,22 +1,23 @@
 from datetime import datetime
 import os
+import pathlib
 import re
+import subprocess
+import sys
+from urllib.parse import quote
 
 import frontmatter
-import mistletoe
+# import mistletoe
 from unidecode import unidecode
 
-os.chdir('/d/Users/an/Documents/__dev/writing/_posts')
+# os.chdir('/d/Users/an/Documents/__dev/writing/_posts')
 
 def format_article(name):
   with open(name) as f:
     post = frontmatter.load(f)
 
-  with open(name) as fin:
-    rendered = mistletoe.Document(fin)
   # print(post)
   # print(post['title'])
-
 
   mddate = post.get("date")
   if not mddate:
@@ -24,26 +25,69 @@ def format_article(name):
   else:
     datestr = mddate.strftime('%Y-%m-%d')
 
+
+
   new_name = post['title'].lower()
   new_name = unidecode(new_name)
-  new_name = re.sub(r"[ ]+", "-", f'{datestr}-{new_name}.md')
+  new_name = re.sub(r"[ ?]+", "-", f'{datestr}-{new_name}.md')
+  new_name = os.path.join(*name.split('/')[:-1], new_name)
 
-  os.rename(name, new_name)
+
   return new_name
 
 def main():
-  article_names = os.listdir()
+  # path = '_drafts/'
+  # article_names = os.walk(path, topdown=True, followlinks=True)
+  # images = os.walk('_drafts/assets/writing', topdown=True, followlinks=True )
 
-  for name in article_names:
-    if name[-3:] != '.md': continue
+  imagesobj = pathlib.Path('_posts')
+  images = article_names = imagesobj.rglob('*')
 
-    if (not os.path.isfile(name)
-        or name[0] == '.'): continue
+  renames = {}
+  # for ximage in imagesobj.rglob('*'):
+  #   image = str(ximage)
+  #   if " " in image and "assets" in image:
+  #     old_name = image
+  #     new_name = unidecode(image.replace(" ", "-")).lower()
+  #     new_name = re.sub(r"[,]", "", new_name)
+  #     os.rename(image, new_name)
+  #     renames[image] = new_name
+  #     print(f"renamed: {image} -> {image.replace(' ', '-')}")
 
-    print(name)
-    if (name[4] != '-' and name[7] != '-'):
-      new_name = format_article(name)
-      print(f"formatted: {new_name} <- {name}")
+  renames = {'_posts/assets/writing/I write to find out what I am thinking - Joan Didion.png': '_posts/assets/writing/i-write-to-find-out-what-i-am-thinking---joan-didion.png', '_posts/assets/writing/I write to find out what I think - Stephen King.png': '_posts/assets/writing/i-write-to-find-out-what-i-think---stephen-king.png'}
+
+  for old_path, new_path in renames.items():
+    old_name = old_path.split('/')[-1]
+    old_name_encoded = quote(old_name.split('/')[-1])
+    for oname in imagesobj.rglob('*'):
+      parts = str(oname).split('/')
+      fname = parts[-1]
+      if " " in str(fname): continue
+      if str(fname)[-3:] != '.md': continue
+      content = open(oname, 'r').read()
+      if old_name_encoded not in content:
+        continue
+      print(f'{old_name} -> {old_name_encoded}')
+      with open(str(oname), 'w') as f:
+        new_name = new_path.split('/')[-1]
+        content = content.replace(old_name_encoded, new_name)
+        f.write(content)
+      written = 1
+
+  # for ppath in imagesobj.rglob('*'):
+  #   path = str(ppath)
+  #   fname = path.split('/')[-1]
+  #   print(fname)
+  #   if fname[-3:] != '.md': continue
+
+  #   if (not os.path.isfile(path)
+  #       or fname[0] == '.'): continue
+
+  #   if (fname[4] != '-' and fname[7] != '-'):
+  #     if sys.argv[1] not in fname: continue
+  #     new_path = format_article(path)
+  #     os.rename(path, new_path)
+  #     print(f"formatted draft: {new_path} <- {fname}")
 
 if __name__ == '__main__':
   main()
